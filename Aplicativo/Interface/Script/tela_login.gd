eeeea34857a94b9e6ef558ffc2785aeeda7f122b
JMiguelsilva2003 <jmigueldasilva953@gmail.com>
2023-12-17 14:49:25 -0300

    limitações de chaves

--- Aplicativo/Interface/Script/tela_login.gd
+++ Aplicativo/Interface/Script/tela_login.gd
@@ -11,3 +11,12 @@ func _on_botão_registro_pressed():
 func _on_login_pressed():
 	NetWork._tryLogin($BoxEmail/Email/Usuario.text, $BoxSenha/fundo/Usuario.text)
 	#get_tree().change_scene_to_file("res://Tela/tela_principal.tscn")
+	
+
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.scancode == KEY_BRACELEFT or event.scancode == KEY_BRACERIGHT:
+			event.set_echo(False)
+			event.set_accepted(False)
+			return true
+	return false
